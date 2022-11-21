a804927a4172647aa1dfef33d0bf0a4511ce29d7
muhammedshiffin <muhammedshiffin@gmail.com>
2022-11-21 14:51:37 +0530

    medicone table fix

---
+++ app/Http/Livewire/Tables/StreetTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Tables;
+
+use Livewire\Component;
+
+class StreetTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.tables.street-table');
+    }
+}
