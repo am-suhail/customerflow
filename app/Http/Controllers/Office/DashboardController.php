088e97a7ae5eb9f3c514c762d6c7a9d0b2b35474
muhammedshiffin <muhammedshiffin@gmail.com>
2023-05-13 00:32:44 +0530

    Chart Color Code

--- app/Http/Controllers/Office/DashboardController.php
+++ app/Http/Controllers/Office/DashboardController.php
@@ -246,7 +246,7 @@ class DashboardController extends Controller
 
             $bar_chart_monthly = new InvoiceChart;
             $bar_chart_monthly->labels($month_invoices->keys());
-            $bar_chart_monthly->dataset('Current Year Revenue', 'bar', $month_invoices->values()->map(fn ($data) => $data->map(fn ($invoice) => $invoice->total_amount)->sum()))->color("#0AA674");
+            $bar_chart_monthly->dataset('Current Year Revenue', 'bar', $month_invoices->values()->map(fn ($data) => $data->map(fn ($invoice) => $invoice->total_amount)->sum()))->color("#0AB574");
         }
 
         // Country Wise Sales Chart
